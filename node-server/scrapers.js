4d1760ad345db9c816768076e666f8b4ede51389
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-18 21:03:04 -0500

    import puppeteer and add function to scrape the webpage of input url

---
+++ node-server/scrapers.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer");
+
+const scrapeWebpage = async (url) => {
+  const browser = await puppeteer.launch(); // launch browser
+  const page = await browser.newPage(); // create new page
+  await page.goto(url); // navigate to input url
+
+  const [element] = await page.$x('//*[@id="text"]'); // <--
+  const text = await element.getProperty("textContent");
+  const name = await text.jsonValue();
+
+  //   const [element2] = await page.$x("//*[@id='img']"); // <--
+  const [element2] = await page.$x('//*[@id="img"]'); // <--
+  const src = await element2.getProperty("src");
+  const img = await src.jsonValue();
+
+  //   const [element3] = await page.$x("//*[@id='img']");
+
+  browser.close();
+
+  console.log({ name, img });
+
+  return { name, img };
+};
+
+scrapeWebpage("https://www.youtube.com/@raiders");
+
+// module.exports = {
+//   scrapeWebpage,
+// };
