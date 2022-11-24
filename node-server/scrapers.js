1293b64f8d0058044dd85d5781c5004c7dc245b2
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-24 17:56:06 -0500

    change function declaration

--- node-server/scrapers.js
+++ node-server/scrapers.js
@@ -1,15 +1,16 @@
 const puppeteer = require("puppeteer");
 
-const scrapeWebpage = async (url) => {
+async function scrapeWebpage(url) {
   const browser = await puppeteer.launch(); // launch browser
   const page = await browser.newPage(); // create new page
   await page.goto(url); // navigate to input url
 
-  const [element] = await page.$x('//*[@id="text"]');
+  const [element] = await page.$x("//*[@id='text']");
+  console.log(element);
   const text = await element.getProperty("textContent");
   const name = await text.jsonValue();
 
-  const [element2] = await page.$x('//*[@id="img"]');
+  const [element2] = await page.$x("//*[@id='img']");
   const src = await element2.getProperty("src");
   const img = await src.jsonValue();
 
@@ -18,6 +19,6 @@ const scrapeWebpage = async (url) => {
   console.log({ name, img });
 
   return { name, img };
-};
+}
 
 module.exports = { scrapeWebpage };
